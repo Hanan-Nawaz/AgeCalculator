164ac84f918046264361b636e6aedf8cd996cc62
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-15 18:20:39 +0500

    Project Completed

--- app/src/main/java/com/example/agecalculator/HomeActivity.java
+++ app/src/main/java/com/example/agecalculator/HomeActivity.java
@@ -13,18 +13,27 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
 
-    int current_date = 0;
-    int current_month = 0;
-    int current_year = 0;
-    int date = 0;
-    int month = 0;
-    int year = 0;
+    int current_date ;
+    int current_month ;
+    int current_year ;
+    int date ;
+    int month ;
+    int year ;
+    int years;
+    int months;
+    int days;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,12 @@ public class HomeActivity extends AppCompatActivity {
         ImageButton button = findViewById(R.id.website_link);
         EditText age_text = findViewById(R.id.age);
         TextView textView = findViewById(R.id.btn_calculate);
+        TextView date_tv  = findViewById(R.id.day);
+        TextView month_tv  = findViewById(R.id.month);
+        TextView year_tv  = findViewById(R.id.year);
+        TextView inmonths_tv  = findViewById(R.id.in_months);
+        TextView yinweeks_tv  = findViewById(R.id.in_weeks);
+        TextView indays_tv  = findViewById(R.id.in_days);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +65,7 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Calendar calender = Calendar.getInstance();
-                date = calender.get(Calendar.DATE);
+                date = calender.get(Calendar.DAY_OF_MONTH);
                 month = calender.get(Calendar.MONTH);
                 year = calender.get(Calendar.YEAR);
 
@@ -82,44 +97,59 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Calendar calender = Calendar.getInstance();
-                current_date = calender.get(Calendar.DATE);
+                current_date = calender.get(Calendar.DAY_OF_MONTH);
                 current_month = calender.get(Calendar.MONTH);
                 current_year = calender.get(Calendar.YEAR);
 
-                int month_age = current_month - month;
-                int day_age = current_date - date;
-                int year_age = current_year - year;
-                int day_after_change;
-                int month_after_change;
+                String b_date = date + "/" + month + "/" + year;
+                String t_date = current_date + "/" + current_month + "/" + current_year;
 
-//                if(month_age < 0){
-//                    month_after_change = 12 - month_age;
-//                }
-//                else{
-//                    month_after_change = month_age;
-//                }
-//
-//                if(month_after_change == 1 || month_after_change == 3 || month_after_change == 5 || month_after_change == 7
-//                        || month_after_change == 8 || month_after_change == 10 || month_after_change == 12){
-//                    if(day_age < 0){
-//                        day_after_change = 31 +  day_age;
-//                    }
-//                    else{
-//                        day_after_change = day_age;
-//                    }
-//                }
-//                else{
-//                    if(day_age < 0){
-//                        day_after_change = 30 +  day_age;
-//                    }
-//                    else{
-//                        day_after_change = day_age;
-//                    }
-//                }
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+                try {
+                    Date birth_date = simpleDateFormat.parse(b_date);
+                    Date today_date = simpleDateFormat.parse(t_date);
+
+                    long start_date = birth_date.getTime();
+                    long end_date = today_date.getTime();
+
+                    if(start_date <= end_date){
+                        org.joda.time.Period period = new Period(start_date, end_date, PeriodType.yearMonthDay());
+                        years = period.getYears();
+                        months = period.getMonths();
+                        days = period.getDays();
+
+                        date_tv.setText(String.valueOf(days));
+                        month_tv.setText(String.valueOf(months));
+                        year_tv.setText(String.valueOf(years));
+
+                        inmonths_tv.setText(String.valueOf((years * 12) + months  ));
+                        yinweeks_tv.setText(String.valueOf(((years * 12) + months ) * 4 ));
+                        indays_tv.setText(String.valueOf((((years * 12) + months ) * 4) * 7 ));
+                    }
+                    else{
+                        date_tv.setText("Error");
+                        month_tv.setText("Error");
+                        year_tv.setText("Error");
+
+                        inmonths_tv.setText("Error");
+                        yinweeks_tv.setText("Error");
+                        indays_tv.setText("Error");
+
+                        Toast.makeText(getApplicationContext(), "Birthday must be smaller than current date", Toast.LENGTH_LONG).show();
+
+                    }
+
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
 
 
-                Toast.makeText(getApplicationContext(), month_age + "/" + day_age + "/" + year_age , Toast.LENGTH_LONG).show();
+
+
+
             }
+
         });
 
 
